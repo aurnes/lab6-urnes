4ba95dc6d526a91e098b1ce1ae15103588baa2d7
Abigail Urnes <aurnes@purdue.edu>
2017-12-07 17:32:53 -0500

    Send high score information to frontend

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var yourtime = 0;
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
@@ -15,6 +16,8 @@ router.post('/', function(req, res, next) {
     database  : 'users'
   });
 
+  yourtime = req.body.time;
+
   var que = "INSERT INTO scores (uid, best, date_best, solved) VALUES ('" + req.body.uid + "', '" + req.body.time + "', '2017-12-07', 1) ON DUPLICATE KEY UPDATE solved=solved+1, date_best = IF(VALUES(best) < best, VALUES(date_best), date_best), best = IF(VALUES(best) < best, VALUES(best), best)";
 
   connection.connect()
@@ -23,12 +26,23 @@ router.post('/', function(req, res, next) {
 
   })
 
-  connection.query("SELECT * FROM scores", function (err, rows, fields){
+  var que2 = "SELECT * FROM scores WHERE uid='" + req.body.uid + "'";
+
+  connection.query(que2, function (err, rows, fields){
     if (err) throw err
     console.log(rows);
-
+    //res.setHeader('Content-Type', 'application/json');
+    
+    if (rows[0].best != yourtime){
+	res.json({ "hi": "High score: " + rows[0].best });
+    }else{
+      res.json({ "hi": "New high score!" });
+    }
   })
-  connection.end
+
+
+  connection.end;
+
 
 });
 
